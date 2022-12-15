b64d866f3cbdcad5d74e8660bb7f2ad0a1c75cbe
Gagan Saini <gagansaini10999@gmail.com>
2022-12-15 22:56:48 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>This is my portfolio website</h1>
+      <img className="my-img" src="image/gagan.jpg" alt="" />
+    </div>
+  );
+}
+
+export default App;
