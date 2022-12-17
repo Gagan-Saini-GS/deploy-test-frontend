e00804830956a553c95e0fed65db75d2eded8944
Gagan-Saini-GS <gagansaini10999@gmail.com>
2022-12-17 12:56:42 +0530

    random

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "./Navbar";
 import About from "./About";
 import Contact from "./Contact";
+import Feed from "./Feed";
+import Signup from "./Signup";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
@@ -20,8 +22,10 @@ function App() {
               </div>
             }
           />
+          <Route path="/Feed" element={<Feed />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
       </div>
     </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,11 +8,17 @@ function Navbar() {
         <h1>Gagan Saini</h1>
       </Link>
       <div className="menubar">
+        <Link to="/feed">
+          <h1 className="about">Feed</h1>
+        </Link>
         <Link to="/about">
           <h1 className="about">About</h1>
         </Link>
         <Link to="/contact">
           <h1 className="about">Contact</h1>
+        </Link>
+        <Link to="/signup">
+          <h1 className="about">Signup</h1>
         </Link>
       </div>
     </div>
